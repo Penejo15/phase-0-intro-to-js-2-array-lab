bc12ef9781ba745b1c22c01b81c539d6052dd3d0
peter enejo <petstorm101@gmail.com>
2022-06-12 18:08:40 +0100

    js array lab

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 // Write your solution here!
+let cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat(CatName){
+    cats.push(CatName)
+}
+
+function destructivelyPrependCat(CatName){
+    cats.unshift(CatName)
+}
+
+function destructivelyRemoveLastCat(CatName){
+    cats.pop()
+}
+
+function destructivelyRemoveFirstCat(CatName){
+    cats.shift()
+}
+
+function appendCat(CatName){
+    return [...cats, CatName]
+}
+
+function prependCat(CatName){
+    return [CatName, ...cats]
+}
+
+function removeFirstCat(){
+    return cats.slice(1)
+}
+
+function removeLastCat(){
+    return cats.slice(0, cats.length -1)
+}
